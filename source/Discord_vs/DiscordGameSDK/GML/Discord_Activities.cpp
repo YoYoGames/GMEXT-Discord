ecfc19fe827ec286a1161be2906e3c75239fc7bb
Francisco Dias <fdias@opera.com>
2023-07-21 14:14:09 +0100

    Updated extension to allow for full activity update control

--- source/Discord_vs/DiscordGameSDK/GML/Discord_Activities.cpp
+++ source/Discord_vs/DiscordGameSDK/GML/Discord_Activities.cpp
@@ -14,7 +14,7 @@
 #include <iostream>
 
 
-bool TryStructGetStringMember(RValue* val, const char* key, std::string &result, const char* function = "")
+bool tryStructGetStringMember(RValue* val, const char* key, std::string &result, const char* function = "")
 {
 	RValue* member = YYStructGetMember(val, key);
 	if (member != NULL)
@@ -31,7 +31,7 @@ bool TryStructGetStringMember(RValue* val, const char* key, std::string &result,
 	return false;
 }
 
-bool TryStructGetRealMember(RValue* val, const char* key, double &result, const char* function = "")
+bool tryStructGetRealMember(RValue* val, const char* key, double &result, const char* function = "")
 {
 	RValue* member = YYStructGetMember(val, key);
 	if (member != NULL)
@@ -49,50 +49,213 @@ bool TryStructGetRealMember(RValue* val, const char* key, double &result, const 
 	return false;
 }
 
+bool tryStructGetBoolMember(RValue* val, const char* key, bool& result, const char* function = "")
+{
+	RValue* member = YYStructGetMember(val, key);
+	if (member != NULL)
+	{
+		int kind = KIND_RValue(member);
+		if (kind == VALUE_REAL || kind == VALUE_INT32 || kind == VALUE_INT64 || kind == VALUE_BOOL)
+		{
+			result = BOOL_RValue(member);
+			return true;
+		}
+
+		YYError("%s :: member '%s' must be of type number\n", function, key);
+		return false;
+	}
+	return false;
+}
+
+bool tryStructGetStructMember(RValue* val, const char* key, RValue& result, const char* function = "")
+{
+	RValue* member = YYStructGetMember(val, key);
+	if (member != NULL)
+	{
+		int kind = KIND_RValue(member);
+		if (kind == VALUE_OBJECT)
+		{
+			SET_RValue(&result, member, NULL, 0);
+			return true;
+		}
+
+		YYError("%s :: member '%s' must be of type struct\n", function, key);
+		return false;
+	}
+	return false;
+}
+
+bool tryStructGetInt64Member(RValue* val, const char* key, int64_t& result, const char* function = "")
+{
+	RValue* member = YYStructGetMember(val, key);
+	if (member != NULL)
+	{
+		int kind = KIND_RValue(member);
+		if (kind == VALUE_REAL || kind == VALUE_INT32 || kind == VALUE_INT64 || kind == VALUE_BOOL)
+		{
+			result = YYGetInt64(val, 0);
+			return true;
+		}
+
+		YYError("%s :: member '%s' must be of type number\n", function, key);
+		return false;
+	}
+	return false;
+}
+
 YYEXPORT void Discord_Activities_UpdateActivity(RValue& Result, CInstance* selfinst, CInstance* otherinst, int argc, RValue* arg)
 {
-	RValue* data = YYGetStruct(arg, 0);
+	RValue* activityData = YYGetStruct(arg, 0);
 	
-	if (data == NULL) return;
+	if (activityData == NULL) return;
 
 	discord::Activity activity{};
 
 	std::string text;
-	if (TryStructGetStringMember(data, "details", text, __FUNCTION__))
-	{
-		activity.SetDetails(text.c_str());
-	}
+	double number = 0;
+	bool boolean = false;
 
-	if (TryStructGetStringMember(data, "state", text, __FUNCTION__))
+	if (tryStructGetStringMember(activityData, "state", text, __FUNCTION__))
 	{
 		activity.SetState(text.c_str());
 	}
 
-	if (TryStructGetStringMember(data, "smallImage", text, __FUNCTION__))
+	if (tryStructGetStringMember(activityData, "details", text, __FUNCTION__))
+	{
+		activity.SetDetails(text.c_str());
+	}
+
+	// Timestamps
+	{
+		RValue timestampsData = { 0 };
+		if (tryStructGetStructMember(activityData, "timestamps", timestampsData, __FUNCTION__))
+		{
+			int64_t _timestamp = 0;
+			if (tryStructGetInt64Member(&timestampsData, "startTime", _timestamp, __FUNCTION__))
+			{
+				activity.GetTimestamps().SetStart(_timestamp);
+			}
+
+			if (tryStructGetInt64Member(&timestampsData, "endTime", _timestamp, __FUNCTION__))
+			{
+				activity.GetTimestamps().SetEnd(_timestamp);
+			}
+		}
+	}
+
+	// Assets
+	{
+		RValue assetsData = { 0 };
+		if (tryStructGetStructMember(activityData, "assets", assetsData, __FUNCTION__))
+		{
+			if (tryStructGetStringMember(&assetsData, "smallImage", text, __FUNCTION__))
+			{
+				activity.GetAssets().SetSmallImage(text.c_str());
+			}
+
+			if (tryStructGetStringMember(&assetsData, "smallText", text, __FUNCTION__))
+			{
+				activity.GetAssets().SetSmallText(text.c_str());
+			}
+
+			if (tryStructGetStringMember(&assetsData, "largeImage", text, __FUNCTION__))
+			{
+				activity.GetAssets().SetLargeImage(text.c_str());
+			}
+
+			if (tryStructGetStringMember(&assetsData, "largeText", text, __FUNCTION__))
+			{
+				activity.GetAssets().SetLargeText(text.c_str());
+			}
+		}
+	}
+
+	// Party
+	{
+		RValue partyData = { 0 };
+		if (tryStructGetStructMember(activityData, "party", partyData, __FUNCTION__))
+		{
+			if (tryStructGetStringMember(&partyData, "id", text, __FUNCTION__))
+			{
+				activity.GetParty().SetId(text.c_str());
+			}
+
+			// PartySize
+			RValue partySizeData = { 0 };
+			if (tryStructGetStructMember(&partyData, "size", partySizeData, __FUNCTION__))
+			{
+				if (tryStructGetRealMember(&partyData, "currentSize", number, __FUNCTION__))
+				{
+					activity.GetParty().GetSize().SetCurrentSize((int32_t)number);
+				}
+
+				if (tryStructGetRealMember(&partyData, "maxSize", number, __FUNCTION__))
+				{
+					activity.GetParty().GetSize().SetMaxSize((int32_t)number);
+				}
+
+				activity.GetAssets().SetSmallText(text.c_str());
+			}
+		}
+	}
+
+	// Secrets
+	{
+		RValue secretsData = { 0 };
+		if (tryStructGetStructMember(activityData, "secrets", secretsData, __FUNCTION__))
+		{
+			if (tryStructGetStringMember(&secretsData, "match", text, __FUNCTION__))
+			{
+				activity.GetSecrets().SetMatch(text.c_str());
+			}
+
+			if (tryStructGetStringMember(&secretsData, "join", text, __FUNCTION__))
+			{
+				activity.GetSecrets().SetJoin(text.c_str());
+			}
+
+			if (tryStructGetStringMember(&secretsData, "spectate", text, __FUNCTION__))
+			{
+				activity.GetSecrets().SetSpectate(text.c_str());
+			}
+
+
+		}
+	}
+
+	if (tryStructGetBoolMember(activityData, "instance", boolean, __FUNCTION__))
+	{
+		activity.SetInstance(boolean);
+	}
+
+	// ############# OLD FOR COMPATIBILITY #############
+
+	if (tryStructGetStringMember(activityData, "smallImage", text, __FUNCTION__))
 	{
 		activity.GetAssets().SetSmallImage(text.c_str());
 	}
 
-	if (TryStructGetStringMember(data, "smallText", text, __FUNCTION__))
+	if (tryStructGetStringMember(activityData, "smallText", text, __FUNCTION__))
 	{
 		activity.GetAssets().SetSmallText(text.c_str());
 	}
 
-	if (TryStructGetStringMember(data, "largeImage", text, __FUNCTION__))
+	if (tryStructGetStringMember(activityData, "largeImage", text, __FUNCTION__))
 	{
 		activity.GetAssets().SetLargeImage(text.c_str());
 	}
 
-	if (TryStructGetStringMember(data, "largeText", text, __FUNCTION__))
+	if (tryStructGetStringMember(activityData, "largeText", text, __FUNCTION__))
 	{
 		activity.GetAssets().SetLargeText(text.c_str());
 	}
 
-	double number;
-	if (TryStructGetRealMember(data, "type", number, __FUNCTION__))
+	if (tryStructGetRealMember(activityData, "type", number, __FUNCTION__))
 	{
 		activity.SetType((discord::ActivityType)number);
 	}
+
+	// #################################################
 
 	state.core->ActivityManager().UpdateActivity(activity, [](discord::Result result)
 		{
